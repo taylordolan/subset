ae670f9bdcf334c009439613bc44945814a60463
Taylor Dolan <taylordolan@me.com>
2014-11-28 22:49:37 -0600

    added rudimentary card dealing and scoring

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 var cards = document.getElementById("cards");
 var selectedCards = 0;
+var score = 0;
+var scoreBox = document.getElementById("score");
 
 function setUp() {
     for (var i=0; i<cards.children.length; i++) {
@@ -27,7 +29,33 @@ setUp();
 
 function clearOnCondition() {
     if (selectedCards === 3) {
+        checkIfMatched();
         clearSelected();
+        scoreBox.innerHTML = score;
+    }
+}
+
+function checkIfMatched() {
+    var setOfCards = document.getElementsByClassName("selected");
+    var matched = 0;
+    if (setOfCards[0].classList.contains("styleA")) {
+        for (var i=0; i<setOfCards.length; i++) {
+            if (setOfCards[i].classList.contains("styleA")) {
+                matched++;
+            }
+        }
+    } else if (setOfCards[0].classList.contains("styleB")) {
+        for (var i=0; i<setOfCards.length; i++) {
+            if (setOfCards[i].classList.contains("styleB")) {
+                matched++;
+            }
+        }
+    } else {
+        console.log("error");
+    }
+
+    if (matched == 3) {
+        score++;
     }
 }
 
