cca3255d9a2ad200e40acd4c0c08199c70d4b03c
Taylor Dolan <taylordolan@me.com>
2015-08-22 12:06:57 -0700

    add all cards for the set deck

--- app.js
+++ app.js
@@ -20,17 +20,17 @@ var card1 = {
 }
 var card2 = {
   id: 2,
-  count: 2,
-  fill: 2,
-  color: 2,
+  count: 1,
+  fill: 1,
+  color: 1,
   shape: 2,
   selected: false
 }
 var card3 = {
   id: 3,
-  count: 3,
-  fill: 3,
-  color: 3,
+  count: 1,
+  fill: 1,
+  color: 1,
   shape: 3,
   selected: false
 }
@@ -106,8 +106,560 @@ var card12 = {
   shape: 3,
   selected: false
 }
+var card13 = {
+  id: 13,
+  count: 1,
+  fill: 2,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card14 = {
+  id: 14,
+  count: 1,
+  fill: 2,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card15 = {
+  id: 15,
+  count: 1,
+  fill: 2,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card16 = {
+  id: 16,
+  count: 1,
+  fill: 2,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card17 = {
+  id: 17,
+  count: 1,
+  fill: 2,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card18 = {
+  id: 18,
+  count: 1,
+  fill: 2,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card19 = {
+  id: 19,
+  count: 1,
+  fill: 3,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card20 = {
+  id: 20,
+  count: 1,
+  fill: 3,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card21 = {
+  id: 21,
+  count: 1,
+  fill: 3,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card22 = {
+  id: 22,
+  count: 1,
+  fill: 3,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card23 = {
+  id: 23,
+  count: 1,
+  fill: 3,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card24 = {
+  id: 24,
+  count: 1,
+  fill: 3,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card25 = {
+  id: 25,
+  count: 1,
+  fill: 3,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card26 = {
+  id: 26,
+  count: 1,
+  fill: 3,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card27 = {
+  id: 27,
+  count: 1,
+  fill: 3,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card28 = {
+  id: 28,
+  count: 2,
+  fill: 1,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card29 = {
+  id: 29,
+  count: 2,
+  fill: 1,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card30 = {
+  id: 30,
+  count: 2,
+  fill: 1,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card31 = {
+  id: 31,
+  count: 2,
+  fill: 1,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card32 = {
+  id: 32,
+  count: 2,
+  fill: 1,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card33 = {
+  id: 33,
+  count: 2,
+  fill: 1,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card34 = {
+  id: 34,
+  count: 2,
+  fill: 1,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card35 = {
+  id: 35,
+  count: 2,
+  fill: 1,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card36 = {
+  id: 36,
+  count: 2,
+  fill: 1,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card37 = {
+  id: 37,
+  count: 2,
+  fill: 2,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card38 = {
+  id: 38,
+  count: 2,
+  fill: 2,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card39 = {
+  id: 39,
+  count: 2,
+  fill: 2,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card40 = {
+  id: 40,
+  count: 2,
+  fill: 2,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card41 = {
+  id: 41,
+  count: 2,
+  fill: 2,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card42 = {
+  id: 42,
+  count: 2,
+  fill: 2,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card43 = {
+  id: 43,
+  count: 2,
+  fill: 2,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card44 = {
+  id: 44,
+  count: 2,
+  fill: 2,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card45 = {
+  id: 45,
+  count: 2,
+  fill: 2,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card46 = {
+  id: 46,
+  count: 2,
+  fill: 3,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card47 = {
+  id: 47,
+  count: 2,
+  fill: 3,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card48 = {
+  id: 48,
+  count: 2,
+  fill: 3,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card49 = {
+  id: 49,
+  count: 2,
+  fill: 3,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card50 = {
+  id: 50,
+  count: 2,
+  fill: 3,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card51 = {
+  id: 51,
+  count: 2,
+  fill: 3,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card52 = {
+  id: 52,
+  count: 2,
+  fill: 3,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card53 = {
+  id: 53,
+  count: 2,
+  fill: 3,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card54 = {
+  id: 54,
+  count: 2,
+  fill: 3,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card55 = {
+  id: 55,
+  count: 3,
+  fill: 1,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card56 = {
+  id: 56,
+  count: 3,
+  fill: 1,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card57 = {
+  id: 57,
+  count: 3,
+  fill: 1,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card58 = {
+  id: 58,
+  count: 3,
+  fill: 1,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card59 = {
+  id: 59,
+  count: 3,
+  fill: 1,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card60 = {
+  id: 60,
+  count: 3,
+  fill: 1,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card61 = {
+  id: 61,
+  count: 3,
+  fill: 1,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card62 = {
+  id: 62,
+  count: 3,
+  fill: 1,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card63 = {
+  id: 63,
+  count: 3,
+  fill: 1,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card64 = {
+  id: 64,
+  count: 3,
+  fill: 2,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card65 = {
+  id: 65,
+  count: 3,
+  fill: 2,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card66 = {
+  id: 66,
+  count: 3,
+  fill: 2,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card67 = {
+  id: 67,
+  count: 3,
+  fill: 2,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card68 = {
+  id: 68,
+  count: 3,
+  fill: 2,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card69 = {
+  id: 69,
+  count: 3,
+  fill: 2,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card70 = {
+  id: 70,
+  count: 3,
+  fill: 2,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card71 = {
+  id: 71,
+  count: 3,
+  fill: 2,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card72 = {
+  id: 72,
+  count: 3,
+  fill: 2,
+  color: 3,
+  shape: 3,
+  selected: false
+}
+var card73 = {
+  id: 73,
+  count: 3,
+  fill: 3,
+  color: 1,
+  shape: 1,
+  selected: false
+}
+var card74 = {
+  id: 74,
+  count: 3,
+  fill: 3,
+  color: 1,
+  shape: 2,
+  selected: false
+}
+var card75 = {
+  id: 75,
+  count: 3,
+  fill: 3,
+  color: 1,
+  shape: 3,
+  selected: false
+}
+var card76 = {
+  id: 76,
+  count: 3,
+  fill: 3,
+  color: 2,
+  shape: 1,
+  selected: false
+}
+var card77 = {
+  id: 77,
+  count: 3,
+  fill: 3,
+  color: 2,
+  shape: 2,
+  selected: false
+}
+var card78 = {
+  id: 78,
+  count: 3,
+  fill: 3,
+  color: 2,
+  shape: 3,
+  selected: false
+}
+var card79 = {
+  id: 79,
+  count: 3,
+  fill: 3,
+  color: 3,
+  shape: 1,
+  selected: false
+}
+var card80 = {
+  id: 80,
+  count: 3,
+  fill: 3,
+  color: 3,
+  shape: 2,
+  selected: false
+}
+var card81 = {
+  id: 81,
+  count: 3,
+  fill: 3,
+  color: 3,
+  shape: 3,
+  selected: false
+}
 
-deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12];
+deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52, card53, card54, card55, card56, card57, card58, card59, card60, card61, card62, card63, card64, card65, card66, card67, card68, card69, card70, card71, card72, card73, card74, card75, card76, card77, card78, card79, card80, card81];
 
 function dealCards(n) {
   for(i=0; i<n; i++) {
@@ -235,7 +787,7 @@ renderTable(table);
 
 // END SET UP
 
-var chosen = [card1, card2, card3];
+// var chosen = [card1, card2, card3];
 
 function checkForMatch(array) {
   var match = 0;
@@ -283,6 +835,7 @@ function matchCardElementToObject(cardElement,cardArray) {
   }
 }
 
+// toggles the selected state of a card object
 function toggleSelected(cardObject) {
   if (cardObject.selected === true) {
     cardObject.selected = false;
