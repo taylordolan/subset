0fdbf886ca0406cf959560324642e62c3abb6ed4
Taylor Dolan <taylordolan@me.com>
2016-01-01 19:41:49 -0800

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+// include gulp
+var gulp = require('gulp');
+
+// include plugins
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sass = require('gulp-ruby-sass');
+var uglify = require('gulp-uglify');
+
+//concatenate and minify js files
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+// compile css from scss files
+gulp.task('sass', function() {
+    return sass('src/scss/index.scss', {style: 'compressed'})
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('copy-index', function() {
+    gulp.src('index.html')
+    .pipe(gulp.dest('build/'));
+});
+
+// watch js and scss files
+gulp.task('watch', function() {
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('index.html', ['copy-index']);
+ });
+
+// default task
+gulp.task('default', ['scripts', 'sass', 'copy-index', 'watch']);
